55e1dd8947d8360cf90dae24d27f86b9e853dd38
Marianoantar <marianoantar@gmail.com>
2024-09-20 21:47:47 -0300

    Primer commit

---
+++ src/app/components/projects/projects.component.ts
@@ -0,0 +1,89 @@
+import { Component, OnInit } from '@angular/core';
+import { Project } from '../../models/project';
+import { ProjectService } from '../../services/project.service';
+import { Global } from '../../services/global';
+import { HttpClientModule, HttpErrorResponse } from '@angular/common/http';
+import { CommonModule, NgFor, NgIf } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { EMPTY, tap } from 'rxjs';
+
+
+@Component({
+  selector: 'app-projects',
+  standalone: true,
+  imports: [CommonModule, HttpClientModule, NgIf, NgFor, RouterModule],
+  templateUrl: './projects.component.html',
+  styleUrl: './projects.component.css',
+  providers: [ProjectService]
+})
+export class ProjectsComponent implements OnInit {
+  public projects: Project[] = [];
+  public url: string;
+  public charging:boolean = true;
+
+  constructor (
+    private _projectService: ProjectService
+  ) {
+    this.url =Global.url;
+  };
+
+  ngOnInit() {
+    this.getProjects();
+  }
+
+
+  // async getProjects() {
+  //   try {
+  //     return this._projectService.getProjects().pipe(
+  //       tap((projects: Project[]) => {
+  //         if (projects) {
+  //           this.projects = projects;
+  //           this.charging = false;
+  //         } else {
+  //           console.error("Error al obtener proyectos")
+  //         };
+  //       })
+  //   } catch ((error: HttpErrorResponse) => {
+  //       // Maneje tipos de error específicos según la respuesta de error
+  //       if (error.status === 404) {
+  //         console.error("No se encontraron proyectos");
+  //       } else if (error.status === 500) {
+  //         console.error("Error del servidor al obtener proyectos");
+  //       } else {
+  //         console.error("Error desconocido al obtener proyectos:", error);
+  //       }
+  
+  //       // Devuelva un observable vacío para evitar un procesamiento posterior
+  //       return EMPTY
+  //     }
+  //   )
+  // }
+
+  async getProjects(){
+    this._projectService.getProjects().subscribe(
+      response => {
+        if(response){
+          this.projects = response;
+          this.charging = false;
+          } else {
+            console.error("Error al obtener proyectos");
+            console.log(response)
+            // Mostrar un mensaje de error al usuario
+          };
+      },
+      error => {
+       if (error.status === 404) {
+          console.error("No se encontraron proyectos");
+        } else if (error.status === 500) {
+          console.error("Error del servidor al obtener proyectos");
+        } else {
+          console.error("Error desconocido al obtener proyectos:", error);
+        }
+  
+        // Devuelva un observable vacío para evitar un procesamiento posterior
+        return EMPTY      }
+    );
+  }
+
+
+}
